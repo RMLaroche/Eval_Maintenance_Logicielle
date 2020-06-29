3dc1daec41f579b68bf1f0c2d93dcb04dea82495
RMLaroche <remy.laroche@epsi.fr>
2020-06-29 09:40:48 +0200

    suppression de l'ancien gros test it_works()

--- src/test/java/com/codurance/training/tasks/ApplicationTest.java
+++ src/test/java/com/codurance/training/tasks/ApplicationTest.java
@@ -50,54 +50,7 @@ public final class ApplicationTest {
         applicationThread.interrupt();
         throw new IllegalStateException("The application is still running.");
     }
-
-    @Test(timeout = 1000) public void
-    it_works() throws IOException {
-        execute("show");
-
-        execute("add project secrets");
-        execute("add task secrets Eat more donuts.");
-        execute("add task secrets Destroy all humans.");
-
-        execute("show");
-        readLines(
-            "secrets",
-            "    [ ] 1: Eat more donuts.",
-            "    [ ] 2: Destroy all humans.",
-            ""
-        );
-
-        execute("add project training");
-        execute("add task training Four Elements of Simple Design");
-        execute("add task training SOLID");
-        execute("add task training Coupling and Cohesion");
-        execute("add task training Primitive Obsession");
-        execute("add task training Outside-In TDD");
-        execute("add task training Interaction-Driven Design");
-
-        execute("check 1");
-        execute("check 3");
-        execute("check 5");
-        execute("check 6");
-
-        execute("show");
-        readLines(
-                "secrets",
-                "    [x] 1: Eat more donuts.",
-                "    [ ] 2: Destroy all humans.",
-                "",
-                "training",
-                "    [x] 3: Four Elements of Simple Design",
-                "    [ ] 4: SOLID",
-                "    [x] 5: Coupling and Cohesion",
-                "    [x] 6: Primitive Obsession",
-                "    [ ] 7: Outside-In TDD",
-                "    [ ] 8: Interaction-Driven Design",
-                ""
-        );
-
-        execute("quit");
-    }
+    
     @Test(timeout = 1000) public void
     addProjectTest() throws IOException {
         execute("add project secrets");
